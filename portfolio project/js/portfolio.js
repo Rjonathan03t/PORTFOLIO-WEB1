17381014610bd46d3d46332c951ca5237a3e4302
jonathan rajao <rjonathan2003t@gmail.com>
2023-07-17 12:13:35 +0300

    rename file

---
+++ portfolio project/js/portfolio.js
@@ -0,0 +1,14 @@
+
+
+var typed = new Typed('#typed', {
+    strings: [
+        'Web Designer',
+        'Pilot',
+        'Streamer',
+        'Dev Front-end'
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+
