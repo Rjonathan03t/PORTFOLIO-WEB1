d64dfa489c407986dcca1612355bfbd0cac46a38
jonathan rajao <rjonathan2003t@gmail.com>
2023-07-29 09:08:48 +0300

    add files

---
+++ portfolio project/floix-onepage-resume-bootstrap-template/js/portfolio.js
@@ -0,0 +1,18 @@
+const navbar = document.getElementById('navbar');  
+const hamburger = document.getElementById('hamburger');  
+ 
+const navhover = document.getElementById('navhover');  
+window.addEventListener('scroll' ,function(){
+    var scroll = window.scrollY;
+		if (scroll > 50) {
+		     navbar.classList.add('fixed');
+		     navhover.classList.add('navhoverFixed');  
+		} else {
+            navbar.classList.remove('fixed');
+            navhover.classList.remove('navhoverFixed');
+		}
+	hamburger.addEventListener('click', function(){
+		navbar.classList.remove('fixed');
+	})
+})
+
---
+++ portfolio project/floix-onepage-resume-bootstrap-template/js/typed.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#typed', {
+    strings: [
+        'Developer',
+        'Pilot',
+        'Streamer',
+        'Designer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 80,
+    loop: true
+});
